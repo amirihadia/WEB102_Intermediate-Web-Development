e661fe89dd743883a95a30c79a8bee7f8848c61f
amirihadia <amirihadia6@gmail.com>
2025-09-23 22:27:27 -0700

    Project required features completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Cards from './components/cards'
+
+const App = () => {
+
+  return(
+    <div className="Title">
+      <h1>World's Best Persian Kitchen</h1>
+      <Cards />
+    </div>
+  )
+}
+export default App
---
+++ src/components/cards.jsx
@@ -0,0 +1,57 @@
+const Cards = () => {
+    return(
+    <div className="cards">
+        
+        <div className="card">
+            <img src="/images/qabli.jpg" alt="Qabli" />
+            <h2>Qabli</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/chicken.jpg" alt="Chicken Kebab" />
+            <h2>Chicken Kebab</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/mantu2.jpg" alt="Manto" />
+            <h2>Manto Dumplings</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/bolani.jpg" alt="Bolani" />
+            <h2>Bolani</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/shami.jpg" alt="Shami Kebab" />
+            <h2>Shami Kebab</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/rawasha.jpeg" alt="Rawasha Kebab" />
+            <h2>Rawasha Kebab</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/shola.jpg" alt="shola" />
+            <h2>Shola</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/qoroti.jpg" alt="qoroti" />
+            <h2>Traditional Qoroti</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/sabzi.jpg" alt="sabzi" />
+            <h2>Sabzi</h2>
+        </div>
+
+        <div className="card">
+            <img src="/images/ash.jpg" alt="ash" />
+            <h2>Ash Noodles</h2>
+        </div>
+    </div>
+    )
+}
+export default Cards;
